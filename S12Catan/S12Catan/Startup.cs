43aee7bbef95b80ea0d7d4ac52cf55eefe7d752f
Rick Vrolings <416994@student.fontys.nl>
2019-10-07 09:27:07 +0200

    Test, Removed a ! from hello world

--- S12Catan/S12Catan/Startup.cs
+++ S12Catan/S12Catan/Startup.cs
@@ -32,7 +32,7 @@ namespace S12Catan
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World");
                 });
             });
         }
